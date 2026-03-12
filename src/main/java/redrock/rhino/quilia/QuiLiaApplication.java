65bb71c6e2597583a6c88c398643ec861f4b0da5
Santiago Romano <sanromano@frba.utn.edu.ar>
2026-03-11 22:36:58 -0300

    initial commit

---
+++ src/main/java/redrock/rhino/quilia/QuiLiaApplication.java
@@ -0,0 +1,13 @@
+package redrock.rhino.quilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiLiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiLiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/redrock/rhino/quilia/QuiLiaApplicationTests.java
@@ -0,0 +1,13 @@
+package redrock.rhino.quilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiLiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
